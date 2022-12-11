0c91419919e12ff30c32d8efb970c2de9099b908
dcibas <dcibas.art@gmail.com>
2022-12-11 23:46:54 +0200

    Java files

---
+++ Month/Menesis.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Menesis {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+    	
+        System.out.println("Iveskite menesi");
+        
+        int menesis = scanner.nextInt();
+        
+        String menesisTekstu = "";
+        
+		if (menesis < 1 || menesis > 12) {
+			System.out.println("Netinkamas skaicius");
+		}
+
+		else if (menesis %2 == 0) {
+				
+		switch(menesis) {
+		
+		case 2: menesisTekstu = "VASARIS";
+		break;
+		case 4: menesisTekstu = "BALANDIS";
+		break;
+		case 6: menesisTekstu = "BIRZELIS";
+		break;
+		case 8: menesisTekstu = "RUGPJUTIS";
+		break;
+		case 10: menesisTekstu = "SPALIS";
+		break;
+		case 12: menesisTekstu = "GRUODIS";
+		break;
+		default: menesisTekstu = "Neteisingas menuo";
+		
+		 }
+		
+	    } else {
+		
+		switch (menesis) {
+		
+		case 1: menesisTekstu = "Sausis";
+		break;
+		case 3: menesisTekstu = "Kovas";
+		break;
+		case 5: menesisTekstu = "Geguze";
+		break;
+		case 7: menesisTekstu = "Liepa";
+		break;
+		case 9: menesisTekstu = "Rugsejis";
+		break;
+		case 11: menesisTekstu = "Lapkritis";
+		break;
+		default: menesisTekstu = "Neteisingas menuo";
+		
+		}
+		
+	  }
+		System.out.println(menesisTekstu);
+	}
+}
