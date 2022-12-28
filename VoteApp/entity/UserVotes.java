f1be73e2bd16df6931397c6b7ffe83a61005748b
dcibas <dcibas.art@gmail.com>
2022-12-29 00:26:48 +0200

    Java Spring files ongoing project

--- VoteApp/entity/UserVotes.java
+++ VoteApp/entity/UserVotes.java
@@ -35,10 +35,6 @@ public class UserVotes {
 	@JoinColumn(name = "vote_post_id")
 	private VotePost votePost;
 	
-//	@ManyToOne
-//	@JoinColumn(name = "vote_counter_id")
-//	private VoteCounter voteCounter;
-	
 	@Enumerated(EnumType.STRING)
 	private VotingChoices votingChoice;
 
--- VoteApp/service/VotePostService.java
+++ VoteApp/service/VotePostService.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.codeacademy.voteapp.dto.UserVotesDto;
